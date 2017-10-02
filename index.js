4c83cd15fd47ce9967f4b04f5dd567cf95b7c60a
Hang Pham <tpham.hang@gmail.com>
2017-10-02 20:02:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ if (name === "Susan") {
 } else {
     console.log("Expect the name to be Susan")
 }
-
+if (height === "70") {
+  consule.log("The height is correct")
+} else {
+  console.log("Expect the height to be 70")
+}
 // Don't worry about this
 module.exports = { name, height
 }
